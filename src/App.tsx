2285242bc71cb75b8a9d797c7e9796cf144e1285
Quynh Chu <98863661+chuq2b2@users.noreply.github.com>
2025-02-27 13:26:40 -0500

    ADD: landing page design and theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,39 @@ import {
   UserButton,
 } from "@clerk/clerk-react";
 
-
 export default function App() {
   return (
-    <header className="flex flex-col items-center justify-center py-8 px-4">
+    <>
       <SignedOut>
-        <div className="text-9xl border-2 p-9 rounded-lg">Welcome to MyCorner</div>
-        <SignInButton />
+        <div className="flex items-center justify-center h-screen w-full">
+          <div className="flex flex-col text-center border-5 border-b-50 p-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="red"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="lucide lucide-video items-end"
+            >
+              <path d="m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5" />
+              <rect x="2" y="6" width="14" height="12" rx="2" />
+            </svg>
+            <div className="p-32">
+              <p className="text-5xl mb-5 font-bold">Welcome to MyCorner</p>
+              <SignInButton />
+            </div>
+          </div>
+        </div>
       </SignedOut>
+
+      {/* Logged-in Page (After Login) */}
       <SignedIn>
         <UserButton />
       </SignedIn>
-    </header>
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 // import { Route } from "lucide-react";
+import { neobrutalism } from "@clerk/themes";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -16,7 +17,14 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <ClerkProvider
+        appearance={{
+          baseTheme: neobrutalism,
+          signIn: { baseTheme: neobrutalism },
+        }}
+        publishableKey={PUBLISHABLE_KEY}
+        afterSignOutUrl="/"
+      >
         <Routes>
           <Route path="/" element={<App />} />{" "}
         </Routes>
