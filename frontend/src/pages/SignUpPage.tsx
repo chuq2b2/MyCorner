e579c6e46b1bb12a0974ef41caf277e301fbbdf8
Quynh Chu <98863661+chuq2b2@users.noreply.github.com>
2025-03-05 22:34:11 -0500

    ADD: test api call

--- frontend/src/pages/SignUpPage.tsx
+++ frontend/src/pages/SignUpPage.tsx
@@ -1,7 +1,22 @@
 import StarBackground from "@/components/StarBackground";
-import { SignUp } from "@clerk/clerk-react";
+import { SignUp, useAuth } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import axios from 'axios';
 
 export default function SignUpPage() {
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    async function sendUserData() {
+      const token = await getToken();
+
+      if (token) {
+        await axios.post("http://localhost:8000/signup", { token });
+      }
+    }
+
+    sendUserData();
+  }, []);
   return (
     <div className="flex items-center justify-center h-screen w-full">
       <StarBackground
